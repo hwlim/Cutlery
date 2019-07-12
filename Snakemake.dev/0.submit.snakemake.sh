9e66bdd7da99552fc809583edc8412ea93c6655d
Lim <bellelavie@gmail.com>
2019-07-12 18:37:18 -0400

    thread parameter

--- Snakemake.dev/0.submit.snakemake.sh
+++ Snakemake.dev/0.submit.snakemake.sh
@@ -2,7 +2,9 @@
 
 nthread=8
 
-bsub -W 6:00 -n $nthread -e submit.err -o submit.out \
+
+#bsub -W 6:00 -n $nthread -e submit.err -o submit.out \
+bsub -W 6:00 -e submit.err -o submit.out \ # no need to setup this because individual job will have their own threads
 	"module load python3/3.6.3
 	snakemake -j $nthread --latency-wait 30 --cluster-config cluster.json  --cluster 'bsub -W {cluster.walltime} -n {cluster.cpu} -M {cluster.memory} -J {cluster.name} -eo {cluster.error} -oo {cluster.output}'"
 
