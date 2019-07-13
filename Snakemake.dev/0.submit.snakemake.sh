e7f3f537c2f50fc4642bc7fc1f6e956117f74d12
HeeWoong Lim <bellelavie@gmail.com>
2019-07-13 14:13:40 -0400

    cluster resource configuration

--- Snakemake.dev/0.submit.snakemake.sh
+++ Snakemake.dev/0.submit.snakemake.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-nthread=8
+nJob=8
 
 
 #bsub -W 6:00 -n $nthread -e submit.err -o submit.out \
 bsub -W 6:00 -e submit.err -o submit.out \ # no need to setup this because individual job will have their own threads
 	"module load python3/3.6.3
-	snakemake -j $nthread --latency-wait 30 --cluster-config cluster.yml  --cluster 'bsub -W {cluster.walltime} -n {cluster.cpu} -M {cluster.memory} -J {cluster.name} -eo {cluster.error} -oo {cluster.output}'"
+	snakemake -j $nJob \
+		--latency-wait 30 \
+		--cluster-config cluster.yml \
+		--cluster 'bsub -W {cluster.walltime} -n {cluster.cpu} -M {cluster.memory} -J {cluster.name} -R {cluster.resource} -eo {cluster.error} -oo {cluster.output}'"
 
