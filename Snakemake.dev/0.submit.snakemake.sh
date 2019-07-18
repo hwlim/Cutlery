00752f82d11deea9741baf6679bb698c08c09e39
Lim <limc8h@bmiclusterp3.chmcres.cchmc.org>
2019-07-18 18:13:37 -0400

    Alpha test complete + MB/GB memory

--- Snakemake.dev/0.submit.snakemake.sh
+++ Snakemake.dev/0.submit.snakemake.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-nJob=8
-totalWallTime="24:00"
+nJob=20
+totalWaitTime="24:00"
+
+snakemake --dag | dot -Tpdf > diag.pdf
 
 
 mkdir -p logs
-#bsub -W 6:00 -n $nthread -e submit.err -o submit.out \
-bsub -W ${totalWallTime} -eo submit.err -oo submit.out \
+bsub -W ${totalWaitTime} -e submit.err -o submit.out \
 	"module load python3/3.6.3
 	snakemake -j $nJob \
 		--latency-wait 30 \
