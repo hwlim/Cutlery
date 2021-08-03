3f8d18383c997ef7eda33d603272bf53341ca770
Lim <bellelavie@gmail.com>
2021-08-03 17:20:38 -0400

    new: script to export processing results

---
+++ cnr.exportResults.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+###########################################3
+# Written by Hee-Wooong Lim
+# 
+
+source $COMMON_LIB_BASE/commonBash.sh
+trap 'if [ `ls -1 ${TMPDIR}/__temp__.$$.* 2>/dev/null | wc -l` -gt 0 ];then rm ${TMPDIR}/__temp__.$$.*; fi' EXIT
+
+function printUsage {
+	echo -e "Usage: `basename $0` (options) <src dir> <des dir>
+Description: Export Cutlery processing results to a destination folder
+Input:
+	A folder containing sample folders that containing outputs, i.e. stratified by samples
+	e.g.
+	3.Sample/
+	├── Ctrl
+	├── dnRAR
+	└── v2
+Output:
+	A folder containing outputs organized by output types and prefixed by sample name
+Options:
+	-f : Overwrite existing files/folders if set" >&2
+}
+
+if [ $# -eq 0 ];then
+	printUsage
+	exit 1
+fi
+
+
+
+###################################
+## option and input file handling
+force=FALSE
+#lengthParam=NULL,NULL
+while getopts ":f" opt; do
+	case $opt in
+		f)
+			force=TRUE
+			;;
+#		s)
+#			optStr=$OPTARG
+#			;;
+		\?)
+			echo "Invalid options: -$OPTARG" >&2
+			printUsage
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			printUsage
+			exit 1
+			;;
+	esac
+done
+
+
+shift $((OPTIND-1))
+if [ $# -lt 2 ];then
+	printUsage
+	exit 1
+fi
+
+srcDir=$1
+desDir=$2
+
+assertDirExist $srcDir
+mkdir -p $desDir
+
+
+echo -e "Exporting Cutlery results" >&2
+echo -e "  src: $srcDir" >&2
+echo -e "  des: $desDir" >&2
+echo -e "  force: $force" >&2
+
+
+sampleL=( `ls -d ${srcDir}/*/` )
+
+## export list
+# - bigwig: all / nfr / nuc
+# - peak & heatmap: all / 1rpm
+# - motif
+
+if [ "$force" == "TRUE" ];then
+	optStr="-f"
+else
+	optStr=""
+fi
+
+for samplePath in ${sampleL[@]}
+do
+	sample=`basename $samplePath`
+
+	# - bigwig: all / nfr / nuc
+	echo -e "1) Exporting bigwig files" >&2
+	mkdir -p ${desDir}/BigWig
+	for frag in all nfr nuc
+	do
+		src=${srcDir}/${sample}/igv.${frag}.ctr.bw
+		des=${desDir}/BigWig/${sample}.${frag}.ctr.bw
+
+		if [ -f $src ];then
+			cp $optStr -v $src $des
+		else
+			echo -e "Warning: $src does not exist, skipping" >&2
+		fi
+	done
+
+	# - peak & heatmap: all / 1rpm
+	echo -e "2) Exporting peak files & heatmaps" >&2
+	mkdir -p ${desDir}/Peak
+	for mode in factor histone
+	do
+		src=${srcDir}/${sample}/HomerPeak.${mode}/peak.exBL.bed
+		des=${desDir}/Peak/${sample}.all.bed
+		if [ -f $src ];then
+			cp $optStr -v $src $des
+		else
+			echo -e "Warning: $src does not exist, skipping" >&2
+		fi
+
+		src=${srcDir}/${sample}/HomerPeak.${mode}/peak.exBL.1rpm.bed
+		des=${desDir}/Peak/${sample}.1rpm.bed
+		if [ -f $src ];then
+			cp $optStr -v $src $des
+		else
+			echo -e "Warning: $src does not exist, skipping" >&2
+		fi
+
+		src=${srcDir}/${sample}/HomerPeak.${mode}/heatmap.exBL.1rpm.png
+		des=${desDir}/Peak/${sample}.1rpm.heatmap.png
+		if [ -f $src ];then
+			cp $optStr -v $src $des
+		else
+			echo -e "Warning: $src does not exist, skipping" >&2
+		fi
+	done
+
+	# - motif
+	echo -e "3) Exporting motif search results" >&2
+	mkdir -p ${desDir}/Motif
+	for src in ${srcDir}/${sample}/Motif/Homer.all  ${srcDir}/${sample}/Motif/MEME.random5k
+	do
+		mode=`basename $src`
+		des=${desDir}/Motif/${sample}.${mode}
+		if [ -d $src ];then
+			cp $optStr -rv $src $des
+		else
+			echo -e "Warning: $src does not exist, skipping" >&2
+		fi
+	done
+done
