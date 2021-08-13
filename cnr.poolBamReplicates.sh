de39c601494d1e9f23580b6585b20b076218c701
Lim <bellelavie@gmail.com>
2021-08-13 17:28:33 -0400

    new: option to handle legacy unsorted bam files; now assume coordinate sorted input bam file in default

--- cnr.poolBamReplicates.sh
+++ cnr.poolBamReplicates.sh
@@ -14,13 +14,16 @@ function printUsage {
 Description:
 	Merge multiple bam files of a group according to sample/group information within a given sample.tsv file
 Input:
-	- sample.tsv file: containing columns 'Name' and 'Group'
+	- sample.tsv file: containing columns 'Name' and 'Group', from original/replicate Cutlery run
 	- src bam directory: contanin replicate bam files
 	- des bam directory: to write merged bam files
 		bam files are named as <group>.bam
 Options:
 	-b: if set, bsub are submitted for merging bam files, default=off
-	-o: if set, overwrite existing destination bam files, default=off" >&2
+	-u: if set, assuming unsorted replicate bam file (name-sorted as in old version of Cutlery), default=off
+		FYI, previosu Cutlery created unsorted bam file.
+		But the current version create coordinate-sorted bam files after alignment
+	-f: if set, force overwrite existing destination bam files, default=off" >&2
 }
 
 if [ $# -eq 0 ];then
@@ -32,13 +35,17 @@ fi
 ###################################
 ## option and input file handling
 bsub=FALSE
+unsortedBam=FALSE
 overwrite=FALSE
-while getopts ":bo" opt; do
+while getopts ":buf" opt; do
 	case $opt in
 		b)
 			bsub=TRUE
 			;;
-		o)
+		u)
+			unsortedBam=TRUE
+			;;
+		f)
 			overwrite=TRUE
 			;;
 		\?)
@@ -84,6 +91,7 @@ echo -e "Pooling replicate bam files" >&2
 echo -e "  - sampleInfo: $sampleInfo" >&2
 echo -e "  - srcDir: $srcDir" >&2
 echo -e "  - desDir: $desDir" >&2
+echo -e "  - unsorted bam: $unsortedBam" >&2
 echo -e "  - bsub:   $bsub" >&2
 
 mkdir -p $desDir
@@ -109,12 +117,18 @@ do
 		echo -e "  - $src" >&2
 	done 2>&1 | tee $log
 
+	if [ "$unsortedBam" == "TRUE" ];then
+		optStr=""
+	else
+		optStr="-S"
+	fi
+
 	if [ "$bsub" == "TRUE" ];then
 		## Parallel processing using HPC:lsf
-		bsub -W 24:00 -n 1 "module load samtools/1.9.0; ngs.concateBamFiles.sh -o $des ${srcL[@]}"
+		bsub -W 24:00 -n 1 "module load samtools/1.9.0; ngs.concateBamFiles.sh $optStr -o $des ${srcL[@]}"
 	else
 		## Sequential processing
-		ngs.concateBamFiles.sh -o $des ${srcL[@]}
+		ngs.concateBamFiles.sh $optStr -o $des ${srcL[@]}
 	fi
 done
 
