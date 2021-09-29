082a0199a7f1060bbfcfda2e161eeb4e1b65aff1
Lim <bellelavie@gmail.com>
2021-09-29 10:45:01 -0400

    new: script to pool replicate fragment

---
+++ cnr.poolFragments.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+###########################################3
+# Cut&Run tools
+# Written by Hee-Wooong Lim
+# 
+# Pool fragments files of multiple replicates by group
+
+source $COMMON_LIB_BASE/commonBash.sh
+trap 'if [ `ls -1 ${TMPDIR}/__temp__.$$.* 2>/dev/null | wc -l` -gt 0 ];then rm ${TMPDIR}/__temp__.$$.*; fi' EXIT
+
+function printUsage {
+	echo -e "Usage: `basename $0` (options) <sample.tsv> <src bam directory> <des bam directory>
+Description:
+	Merge multiple fragment bed files of a group according to sample/group information within a given sample.tsv file
+Input:
+	- sample.tsv file: containing columns 'Name' and 'Group', from original/replicate Cutlery run
+	- src replicate sample directory: contanining sample directories
+	- des pooled sample directory: to write merged <sample>/fragment.bed.gz files
+Options:
+	-t: if set, dry run simply displaying pooling message, default=off
+	-b: if set, bsub are submitted for merging bam files, default=off
+	-u: if set, input/output fragment files are assumed to be 'unsorted', default=off
+	-f: if set, force overwrite existing destination bam files, default=off" >&2
+}
+
+if [ $# -eq 0 ];then
+	printUsage
+	exit 1
+fi
+
+
+###################################
+## option and input file handling
+testOnly=FALSE
+bsub=FALSE
+unsorted=FALSE
+overwrite=FALSE
+while getopts ":bfut" opt; do
+	case $opt in
+		t)
+			testOnly=TRUE
+			;;
+		b)
+			bsub=TRUE
+			;;
+		u)
+			unsorted=TRUE
+			;;
+		f)
+			overwrite=TRUE
+			;;
+		\?)
+			echo "Invalid options: -$OPTARG" >&2
+			printUsage
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			printUsage
+			exit 1
+			;;
+	esac
+done
+
+
+shift $((OPTIND-1))
+if [ $# -lt 3 ];then
+	printUsage
+	exit 1
+fi
+sampleInfo=$1
+srcDir=$2
+desDir=$3
+
+assertFileExist $sampleInfo
+assertDirExist $srcDir
+
+###################################
+## main code
+
+groupL=`tail -n +2 $sampleInfo | grep -v -e ^$ -e ^# | cut -f 3 | sort | uniq`
+
+echo -e "Pooling replicate fragment files" >&2
+echo -e "  - sampleInfo: $sampleInfo" >&2
+echo -e "  - srcDir:   $srcDir" >&2
+echo -e "  - desDir:   $desDir" >&2
+echo -e "  - bsub:     $bsub" >&2
+echo -e "  - unsorted: $unsorted" >&2
+echo -e "  - testOnly: $testOnly" >&2
+
+mkdir -p $desDir
+for group in ${groupL[@]}
+do
+	des=${desDir}/${group}/fragment.bed.gz
+	log=${desDir}/${group}/fragment.log
+
+	## Checking existing destination file
+	if [ -f $des ] && [ "$overwrite" != "TRUE" ];then
+		echo -e "Warning: $des already exists, pass" >&2
+		continue
+	fi
+
+	## List of replicate bam files
+	srcL=( `tail -n +2 $sampleInfo | grep -v -e ^$ -e "^#" | gawk '{ if($3 == "'$group'") printf "'$srcDir'/%s/fragment.bed.gz\n", $2 }'` )
+	assertFileExist ${srcL[@]}
+
+	## Merging to destination
+	echo -e "Creating $des" >&2
+	for src in ${srcL[@]}
+	do
+		echo -e "  - $src" >&2
+	done
+
+	## test only
+	[ "$testOnly" == "TRUE" ] && continue
+
+
+	## actual pooling
+	mkdir -p ${desDir}/${group}
+	echo -ne $log
+	for src in ${srcL[@]}
+	do
+		echo -e "- $src" >> $log
+	done
+
+
+	if [ "$unsorted" == "TRUE" ];then
+		if [ "$bsub" == "TRUE" ];then
+			## Parallel processing using HPC:lsf
+			bsub -W 24:00 -n 1 "cat ${srcL[@]} > $des"
+		else
+			## Sequential processing
+			cat ${srcL[@]} > $des
+		fi
+	else
+		inputStr=""
+		for src in ${srcL[@]}
+		do
+			inputStr="${inputStr} <( zcat $inputStr )"
+		done
+
+		if [ "$bsub" == "TRUE" ];then
+			## Parallel processing using HPC:lsf
+			bsub -W 24:00 -n 1 "sort -m -k1,1 -k2,2n -k3,3n ${inputStr} | gzip > $des"
+		else
+			## Sequential processing
+			sort -m -k1,1 -k2,2n -k3,3n ${inputStr} | gzip > $des
+		fi
+	fi
+done
+
