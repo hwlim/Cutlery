d34cc2b586fca3d3e889e10868d89224b6992b63
AHNSF9 <christopher.ahn@cchmc.org>
2021-10-19 06:51:23 -0400

    Report Update

--- Script/cnr.createReportHTML.r
+++ Script/cnr.createReportHTML.r
@@ -25,8 +25,8 @@ arguments <- parse_args(parser, positional_arguments = TRUE)
 
 # Option handling
 opt=arguments$options
-sampDir=opt$sampleDir
-qcDir=opt$qcDir
+sampDir=paste0(getwd(), "/", opt$sampleDir)
+qcDir=paste0(getwd(), "/", opt$qcDir)
 outputFile=opt$outputFile
 
 #Read in and crop logo
@@ -34,7 +34,6 @@ logo = paste0(Sys.getenv("CUTLERY"), "/Plan/logo.png")
 logo = image_read(logo)
 logo = image_scale(logo, "200")
 tempLogo = tempfile()
-# logo = image_crop(logo, "240x100+33+10")
 image_write(logo, path = tempLogo, format = "png")
 
 #count peaks
@@ -49,13 +48,34 @@ sumPeaks=function(bed) {
 	return(sum(strtoi(getDiffs)))
 }
 
+#get % of fragments that intersect at least one peak
+fragIntersect=function(fragBed, bed) {
+	tempFragBed <- tempfile()
+
+	#create temporary unzipped frag file
+	system(paste0("zcat ", fragBed, " > ", tempFragBed))
+
+	#count fragments
+	countLines <- system(paste0("wc -l ", tempFragBed), intern = TRUE, ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
+	print(countLines)
+	numFrags <- strtoi(strsplit(countLines, "\\s+")[[1]][1])
+	print(numFrags)
+
+	#get number of intersects
+	getIntersects <- system(paste0("bedtools intersect -u -a ", tempFragBed, " -b ", bed, " | wc -l"), intern = TRUE, ignore.stdout = FALSE, ignore.stderr = FALSE, wait = TRUE)
+	numIntersects <- strtoi(strsplit(getIntersects, "\\s+")[[1]][1])
+	unlink(tempFragBed)
+	return(numIntersects / numFrags * 100)
+}
+
 #make two tables, one each for histone mode and factor mode
-histoneTable <- data.table(Sample=character(), peakcount=numeric(), widths=numeric())
-factorTable <- data.table(Sample=character(), peakcount=numeric())
+histoneTable <- data.table(Sample=character(), peakcount=numeric(), widths=numeric(), fraction=numeric())
+factorTable <- data.table(Sample=character(), peakcount=numeric(), fraction=numeric())
 
 #make a copy of the original Rmd file
 template <- paste0(Sys.getenv("CUTLERY"), "/Script/cnr.createReportHTMLTemplate.Rmd")
-system(sprintf("cp %s Report.Rmd", template))
+tempTemplate <- tempfile(fileext=".Rmd")
+system(sprintf("cp %s %s", template, tempTemplate))
 
 #create separate coverage html files for each sample
 samplePaths <- list.files(sampDir)
@@ -68,6 +88,7 @@ for (sample in sampleQC) {
 
 	#get path to sample Directory
 	sampleDirectory <- paste(head(unlist(strsplit(sample, "/")), -1), collapse = "/")
+	fragBed <- paste0(sampleDirectory, "/fragment.bed.gz")
 	fileList <- list.files(sampleDirectory)
 	
 	#get heatmap and coverage
@@ -83,11 +104,13 @@ for (sample in sampleQC) {
 	if (factorMode == "factor") {
 		heatmap <- list.files(homerFolderPath, pattern = "*heatmap.exBL.1rpm.png", full.names = TRUE)
 		peakFile <- list.files(homerFolderPath, pattern = "*peak.exBL.1rpm.bed", full.names = TRUE)
-		factorTable <- rbind(factorTable, list(sampleName, countPeaks(peakFile)))
+		intersectPerc <- signif(fragIntersect(fragBed, peakFile), digits=3)
+		factorTable <- rbind(factorTable, list(sampleName, countPeaks(peakFile), paste0(intersectPerc, "%")))
 	} else {
 		heatmap <- list.files(homerFolderPath, pattern = "*heatmap.exBL.png", full.names = TRUE)
 		peakFile <- list.files(homerFolderPath, pattern = "*peak.exBL.bed", full.names = TRUE)
-		histoneTable <- rbind(histoneTable, list(sampleName, countPeaks(peakFile), sumPeaks(peakFile)))
+		intersectPerc <- signif(fragIntersect(fragBed, peakFile), digits=3)
+		histoneTable <- rbind(histoneTable, list(sampleName, countPeaks(peakFile), sumPeaks(peakFile),  paste0(intersectPerc, "%")))
 	}
 
 	#get peak-examples png file
@@ -97,41 +120,38 @@ for (sample in sampleQC) {
 	#add heatmap at the end
 	#This is required as tabs need to be generated based on the number of samples in the sample.tsv file
 	line = paste0("### ", sampleName, " {.tabset}")
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line="```{r, echo=FALSE, out.width='100%', fig.align='center', message=FALSE, warning=FALSE}"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = paste0("knitr::include_graphics(\"",peakExamplePlot,"\")")
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "```"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 
 	line = "<br />"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 
 	line="```{r, echo=FALSE, out.width='70%', fig.align=\"center\"}"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = paste0("knitr::include_graphics(\"",heatmap,"\")")
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "```"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 
 	line = "\n"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "\n"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "<br />"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "\n"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 	line = "\n"
-	write(line,file="Report.Rmd",append=TRUE)
+	write(line,file=tempTemplate,append=TRUE)
 }
 
-#create master Report Template
-#includes everything except for genome coverage plots
-rmarkdown::render('Report.Rmd', output_file = paste0(outputFile, ".html"))
-
-#Delete intermediate coverage Reports and plots
-system(sprintf("rm Report.Rmd"))
+#Render report
+rmarkdown::render(tempTemplate, output_file = paste0(outputFile, ".html"), output_dir = getwd())
 
 unlink(tempLogo)
+unlink(tempTemplate)
