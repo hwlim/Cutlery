00005ee77c2987e1fd219b33a8a820fed691b374
AHNSF9 <christopher.ahn@cchmc.org>
2021-10-22 09:35:48 -0400

    folder path update

--- Script/cnr.createReportHTML.r
+++ Script/cnr.createReportHTML.r
@@ -25,8 +25,8 @@ arguments <- parse_args(parser, positional_arguments = TRUE)
 
 # Option handling
 opt=arguments$options
-sampDir=paste0(getwd(), "/", opt$sampleDir)
-qcDir=paste0(getwd(), "/", opt$qcDir)
+sampDir=paste0(normalizePath(opt$sampleDir))
+qcDir=paste0(normalizePath(opt$qcDir))
 outputFile=opt$outputFile
 
 #Read in and crop logo
