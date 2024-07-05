619fe3928e36d644e9a82e4ebb8c8ff3c18fef72
Lim <bellelavie@gmail.com>
2024-07-05 14:03:12 -0400

    update: new option to calculate/visualizez average fragment length by wma

--- Script/ngs.fragLenHist.r
+++ Script/ngs.fragLenHist.r
@@ -8,9 +8,11 @@ suppressPackageStartupMessages(library('tools', quiet=TRUE))
 suppressPackageStartupMessages(library('ggplot2', quiet=TRUE))
 suppressPackageStartupMessages(library('cowplot', quiet=TRUE))
 suppressPackageStartupMessages(library('optparse', quiet=TRUE))
-suppressPackageStartupMessages(library('plotly', quiet=TRUE))
+#suppressPackageStartupMessages(library('plotly', quiet=TRUE))
+require(reshape2)
 
 source(sprintf("%s/basicR.r", Sys.getenv("COMMON_LIB_BASE")))
+source(sprintf("%s/commonR.r", Sys.getenv("COMMON_LIB_BASE")))
 
 
 # command line option handling
@@ -19,8 +21,8 @@ option_list <- list(
 	make_option(c("-l","--maxLen"), default=1000, help="Max fragment length, x-axis for plotting. default=1000"),
 	make_option(c("-n","--name"), default=NULL, help="Sample name to display at top. default=<input file name>"),
 	make_option(c("-i","--interactive"), default=FALSE, action="store_true", help="If set, interactive plotly plot is also generated in html"),
-	make_option(c("-c","--cSorted"), default=FALSE, action="store_true", help="If set, input bam file is assumed to be coordinate-sorted not name-sorted")
-#	make_option(c("-s","--size"), default="600,600", help="Comma-separated figure size, xSize,ySize"),
+	make_option(c("-c","--cSorted"), default=FALSE, action="store_true", help="If set, input bam file is assumed to be coordinate-sorted not name-sorted"),
+	make_option(c("-a","--getAverage"), default=FALSE, action="store_true", help="IF set, return average fragment length by WMA will be returned to STDOUT and highlighted in the plot")
 #	make_option(c("-f","--field"), default="", help="Comma-separated field numbers for x-axis, y-axis."),
 )
 parser <- OptionParser(usage = "%prog [options] <bam or bed.gz>",
@@ -52,14 +54,18 @@ maxLen=opt$maxLen
 name=opt$name
 drawPlotly = opt$interactive
 cSorted=opt$cSorted
+getAverage=opt$getAverage
 assertFileExist(src)
 
 
 if(FALSE){
-	src="Sox2.frag.bed.gz"
+	src="1.1.Align/DE_H3K9me3_Control_A6/align.sortByName.bam"
 	maxLen=1000
-	outPrefix=NULL	
-	des.dist="fragLen.dist.txt"
+	outPrefix="test"
+	cSorted=FALSE
+	getAverage=TRUE
+	name="test"
+	drawPlotly=FALSE
 }
 
 if(is.null(name)) name=src
@@ -90,11 +96,8 @@ des.dist = sprintf("%s.txt", outPrefix)
 des.hist = sprintf("%s.png", outPrefix)
 	
 write(sprintf("  - %s", src), stderr())
-#if( mode=="bam" ){
-#	cmd=sprintf("bamToBed -bedpe -i %s 2>&1 | grep ^chr | gawk 'BEGIN{printf \"fragLen\\tCnt\\n\"; maxLen=%d }{ if($1==\".\" || $3==\".\") next; d=$6-$2; if(d>maxLen){d=maxLen}; cnt[d]++ }END{ for( i=1;i<=maxLen;i=i+1 ) printf \"%%d\\t%%d\\n\", i, cnt[i] }' > %s", src, maxLen, des.dist)
-#}else{
-#	cmd=sprintf("zcat %s | grep ^chr | gawk 'BEGIN{printf \"fragLen\\tCnt\\n\"; maxLen=%d }{ d=$3-$2; if(d>maxLen){d=maxLen}; cnt[d]++ }END{ for( i=1;i<=maxLen;i=i+1 ) printf \"%%d\\t%%d\\n\", i, cnt[i] }' > %s", src, maxLen, des.dist)
-#}
+
+## Checking fragment length distribution
 if(cSorted){
 	cmd=sprintf("ngs.getFragLenHist.sh -o %s -l %d -c %s", des.dist, maxLen, src)
 }else{
@@ -103,14 +106,50 @@ if(cSorted){
 system(cmd)
 data.dist = read.delim(des.dist, header=TRUE)
 
-g1 = ggplot(data=data.dist, aes(x=fragLen, y=Cnt/1000)) + geom_line() +
-	labs(title=name, x="Fragment Length", y="Frequency (x1000)")
-g2 = ggplot(data=data.dist, aes(x=fragLen, y=log10(Cnt))) + geom_line() +
-	labs(title=name, x="Fragment Length", y="log10(Frequency)")
+## Caculate average fragment length by WMA
+## i.e. finding fragment length with maximum frequency after WMA
+data.wma = data.dist
+data.wma[nrow(data.wma),2] = 0
+data.wma[,2] = wma(data.wma[,2],5)
+avg = round(which.max(data.wma[,2]))
+
+
+
+## Draw density plots: linear and log scale
+if( getAverage ){
+	# print to STDOUT
+	write(avg, stdout())
+	# Plot
+	data.plot = data.frame(data.dist, WMA=data.wma[,2])
+	colnames(data.plot) = c("FragLen","Raw","WMA")
+	data.melt = melt(data.plot, measure.vars=c("Raw","WMA"))
+	colnames(data.melt) = c("FragLen","Category","Count")
+	g1 = ggplot(data=data.melt, aes(x=FragLen, y=Count/1000, colour=Category)) +
+		geom_line() +
+		labs(title=sprintf("%s (avg = %d bp)", name, avg), x="Fragment Length", y="Frequency (x1000)") +
+		geom_vline(xintercept=avg, linetype="dashed", color="purple", linewidth=0.5)
+
+	g2 = ggplot(data=data.melt, aes(x=FragLen, y=log10(Count), colour=Category)) +
+		geom_line() +
+		labs(title=sprintf("%s (avg = %d bp)", name, avg), x="Fragment Length", y="Frequency (x1000)") +
+		geom_vline(xintercept=avg, linetype="dashed", color="purple", linewidth=0.5)
+}else{
+	g1 = ggplot(data=data.dist, aes(x=fragLen, y=Cnt/1000)) +
+		geom_line() +
+		labs(title=name, x="Fragment Length", y="Frequency (x1000)")
+	g2 = ggplot(data=data.dist, aes(x=fragLen, y=log10(Cnt))) +
+		geom_line() +
+		labs(title=name, x="Fragment Length", y="log10(Frequency)")
+}
 g = plot_grid(g1, g2, nrow=2)
 ggsave(des.hist, g, width=6, height=6)
 
 
+
+
+
+
+## Draw interactive density plots
 if(drawPlotly){
 	des.html = sprintf("%s.html", outPrefix)
 	fig <- plot_ly(data.dist, x = ~fragLen, y = ~Cnt, type = 'scatter', mode = 'lines')
