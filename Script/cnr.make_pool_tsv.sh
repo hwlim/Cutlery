54c6feb546f1d12e57886edcebda1f7eab0edd35
Lim <bellelavie@gmail.com>
2025-10-29 12:17:14 -0400

    update: skip peakOpt column

--- Script/cnr.make_pool_tsv.sh
+++ Script/cnr.make_pool_tsv.sh
@@ -6,6 +6,6 @@ tsv=$1
 
 assertFileExist $tsv
 
-grep -v ^# $tsv | head -n 1
+grep -v ^# $tsv | head -n 1 | cut -f 1-7
 grep -v ^# $tsv | grep -v ^$ | tail -n +2 | cut -f 3,7 | sort -k1,1 | uniq |  gawk '{ printf "%s\t%s\t%s\tNULL\tNULL\tNULL\t%s\n", $1, $1, $1, $2 }' 
 
